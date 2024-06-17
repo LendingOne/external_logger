b4f96f726fcbd96d341602de39abb485839eda81
Artur Gin <genyceny@gmail.com>
2024-06-17 13:57:49 +0400

    initial commit

---
+++ spec/external_logger_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe ExternalLogger do
+  it "has a version number" do
+    expect(ExternalLogger::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
